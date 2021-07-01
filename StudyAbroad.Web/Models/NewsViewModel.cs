5ffd38545bba8a6b35d9f4dc650ba8ba456d7f92
lethihangtk15 <70497474+lethihangtk15@users.noreply.github.com>
2021-07-01 15:44:50 +0700

    Add project files.

---
+++ StudyAbroad.Web/Models/NewsViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudyAbroad.Web.Models
+{
+    public class NewsViewModel
+    {
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Bạn cần nhập tiêu đề tin tức")]
+        [StringLength(120, ErrorMessage = "tiêu đề tin tức không quá 120 ký tự")]
+        public string Name { get; set; }
+
+
+        [Required(ErrorMessage = "Bạn cần chọn danh mục")]
+        public Nullable<int> CategoryID { get; set; }
+
+        public string Description { get; set; }
+
+        [AllowHtml]
+        public string Content { get; set; }
+        public string Image { get; set; }
+        public Nullable<int> Status { get; set; }
+        public Nullable<System.DateTime> CreateDate { get; set; }
+        public Nullable<System.DateTime> ModifiedDate { get; set; }
+
+        public HttpPostedFileWrapper LogoFile { get; set; }
+    }
+}
